b382c200b1c71bec24d5ef661313d881d6ab11a0
tkabir3066 <tutulkabir@gmail.com>
2026-01-03 10:04:46 +0530

    feat: Prisma error handling part-1 and part-2 completed

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
@@ -7,10 +8,39 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  let statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+  let statusCode: number = StatusCodes.INTERNAL_SERVER_ERROR;
   let success = false;
   let message = err.message || "Something went wrong!";
   let error = err;
+
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      message = "Duplicate Key error";
+      error = err.meta;
+      statusCode = StatusCodes.CONFLICT;
+    }
+    if (err.code === "1001") {
+      message = "Authentication failed against database server";
+      error = err.meta;
+      statusCode = StatusCodes.BAD_GATEWAY;
+    }
+    if (err.code === "2003") {
+      message = "Foreign key constraint failed on";
+      error = err.meta;
+    }
+  } else if (err instanceof Prisma.PrismaClientValidationError) {
+    message = "Validation Error";
+    error = err.message;
+    statusCode = StatusCodes.BAD_REQUEST;
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    message = "Unknown prisma error required";
+    error = err.message;
+    statusCode = StatusCodes.BAD_REQUEST;
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    message = "Prisma Client failed to initialized!";
+    error = err.message;
+    statusCode = StatusCodes.BAD_REQUEST;
+  }
 
   res.status(statusCode).json({
     success,
